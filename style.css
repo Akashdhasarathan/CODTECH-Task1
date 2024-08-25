c2736868f5e3bae6b75e756e8734f6114dd83fba
Akashdhasarathan <146057736+Akashdhasarathan@users.noreply.github.com>
2024-08-25 06:14:20 -0700

    Create style.css

---
+++ style.css
@@ -0,0 +1,129 @@
+*{
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+}
+.container{
+    width: 100%;
+    min-height: 100vh;
+    background: linear-gradient(135deg,#4372cf,#6dd63c);
+    padding: 10px;
+}
+
+.todo{
+    width: 100%;
+    max-width: 540px;
+    background:rgb(148, 189, 192);
+    margin: 100px auto 20px;
+    padding: 40px 30px 70px;
+    border-radius: 10px;
+    position: relative;
+
+}
+.todo i{
+    position: absolute;
+    top: 51px;
+    left:190px;
+    font-size: 35px;
+    color: #002765;
+}
+.todo:hover {
+    box-shadow: 0px 10px 20px 20px #a2a3a3;
+
+}
+.todo h2{
+    color: #002765;
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px; 
+    border:3px solid #a2a3a3;
+    border-radius: 4px;
+    margin-right: 330px;
+    padding: 10px 15px; 
+}
+.todo:hover h2{
+    background-color: #a2a3a3;
+}
+
+.row{
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #a2a3a3;
+    border-radius: 30px;
+    padding-left: 20px;
+    margin-bottom: 25px;
+
+}
+input{
+    flex:1;
+    border: none;
+    outline: none;
+    background: transparent;
+    padding: 10px;
+}
+button{
+    border: none;
+    outline: none;
+    padding: 16px 50px;
+    background-color:#002765;
+    color: #fff;
+    font-size: 16px;
+    cursor: pointer;
+    border-radius: 40px;
+
+}
+button:hover{
+    transform: scale(1.1);
+}
+
+ul li{
+    list-style: none;
+    font-size: 17px;
+
+    padding: 12px 8px 12px 50px;
+    user-select: none;
+    cursor: pointer;
+    position: relative;
+
+}
+ul li::before{
+    content: ' ';
+    position: absolute;
+    height: 28px;
+    width: 28px;
+    border-radius: 50%;
+    background-color: #a2a3a3;
+    top: 12px;
+    left: 8px;
+
+
+}
+
+ul li.checked{
+    color: #555;
+    text-decoration:underline;
+}
+ul li.checked::before{
+    background-color:#002765;
+}
+ul li span{
+    position: absolute;
+    right: 0;
+    top: 5px;
+    width: 40px;
+    height: 40px;
+    font-size: 32px;
+    color: #555;
+    line-height: 40px;
+    text-align: center;
+    border-radius: 50%;
+    
+}
+ul li span:hover{
+    background-color:#a2a3a3;
+}
+
+
+
+
